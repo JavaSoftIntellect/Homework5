b674f5aa1a452c8de5e59bc35def609a14b03f6f
Remzi <remzitdf@abv.bg>
2016-06-13 17:27:22 +0300

    ok

---
+++ src/Main3.java
@@ -0,0 +1,15 @@
+
+
+public class Main3 {
+	static void param(int a, int b, int c) {
+		System.out.println(a + "*" + b + "*" + c + "=" + a * b * c);
+	}
+
+	public static void main(String[] args) {
+		param(2,8,4);
+		param(3,-7,5);
+		param(2,3,2);
+		param(-1,-8,1);
+		param(4,-4,4);
+		}
+}
